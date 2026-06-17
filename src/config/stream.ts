8f0c75a231321baf20d1cfdcf599d8cd7916f00a
rezvX <rezveeparvez@gmail.com>
2026-06-18 04:10:15 +0600

    Updated Schedule

--- src/config/stream.ts
+++ src/config/stream.ts
@@ -10,20 +10,12 @@ export const streamConfig = {
 export const upcomingSchedule = [
   {
     id: "s1",
-    title: "Portugal VS DR Congo",
-    description: "FIFA World Cup 2026 Group Stage Match",
-    date: "2026-06-17",
+    title: "Argentina VS Austria",
+    description: "Group Stage Match",
+    date: "2026-06-22",
     time: "11:00 PM",
     timezone: "BST+6",
-    tag: "Football",
+    tag: "FIFA World Cup 2026",
   },
-  {
-    id: "s2",
-    title: "England VS Coratia",
-    description: "FIFA World Cup 2026 Group Stage Match",
-    date: "2026-06-18",
-    time: "02:00 AM",
-    timezone: "BST+6",
-    tag: "Football",
-  },
+  
 ];
